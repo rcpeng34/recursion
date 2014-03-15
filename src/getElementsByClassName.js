accdee381df68c804b664d3558ef28ba5fa943ab
rcpeng34 <rcpeng@gmail.com>
2014-03-15 17:53:50 -0400

    implemented getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,26 @@
 // };
 
 // But in stead we're going to implement it from scratch:
-var getElementsByClassName = function (className) {
-  // your code here
+var getElementsByClassName = function (className, rootNode) {
+  var resultArray = [];
+// checks to see if rootNode is provided
+// if not then it is original call on document
+  if(rootNode === undefined) {
+    rootNode = document.body;
+  };
+// since text nodes have no classList, check if classList is undefined
+// checks rootNode to see if className is in classList
+// if node contains className, pushes to resultArray
+  if(rootNode.classList !== undefined && rootNode.classList.contains(className)) {
+    resultArray.push(rootNode);
+  };
+// call getElementsByClassName on all childe elements
+  for (var i = 0; i < rootNode.childNodes.length; i++) {
+    var concatArray = (getElementsByClassName(className, rootNode.childNodes.item(i)));
+    if (concatArray.length>0) {
+      resultArray = resultArray.concat(concatArray);
+    };
+  };
+
+  return resultArray;
 };
